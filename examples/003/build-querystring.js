791b1168ba4452a3131a9a01fa495bc8f91d7519
Nelson Moller <nmoller.c@gmail.com>
2019-11-04 13:20:53 -0500

    003: Querystring Se familiariser avec querystring. On execute par docker .

--- examples/003/build-querystring.js
+++ examples/003/build-querystring.js
@@ -1,1 +1,12 @@
 const querystring = require('querystring');
+
+const apiHost = 'https://jira.uqam.ca/rest/api/latest/search?jql=';
+
+const jqlParams = {
+  assignee: 'moller_n',
+  startAt: 2,
+  maxResults: 2
+};
+
+const apiUrl = `${apiHost}"${querystring.stringify(jqlParams)}"`;
+console.log(`My call is: ${apiUrl}`);
--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,8 @@
 const querystring = require('querystring');
+
+const url = 'http://www.opencanvas.co.uk?test01=Nelson&test02=toto&test03=titi';
+const parsedUrl = querystring.parse(url.substring(url.indexOf('?') + 1));
+
+console.log(`Test01: ${parsedUrl.test01}`);
+console.log(`Test02: ${parsedUrl.test02}`);
+console.log(`Test03: ${parsedUrl.test03}`);
